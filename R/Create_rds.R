97f22de909cd8d3efa0e8e1cbc013b4cc09da192
itblock <wnsuddlsla@gmail.com>
2019-09-19 13:22:46 +0900

    Fix error code.

--- R/Create_rds.R
+++ R/Create_rds.R
@@ -16,73 +16,72 @@ create_rds<- function(connectionDetails){
         work_dir <- readline("Set work directory path : ")
     }
     connection <- DatabaseConnector::connect(connectionDetails)
-    tryCatch(save_data(work_dir,connection), error = function(e){
-        e
-        disconnect(connection)
+    schema_name <- connectionDetails$schema
+    tryCatch(save_data(connection, work_dir, schema_name),
+             error = function(e){
+                 print(e)
+                 disconnect(connection)
     })
 
 }
 
-save_data <- function(work_dir,connection){
-    dir.create(file.path(work_dir, "Gemini RDS"), showWarnings = FALSE)
-    work_dir <- file.path(work_dir, "Gemini RDS","/")
-    ################################################################################
-    # PERSON DATA SAVING FUNCTION
-    ################################################################################1
-    cat("Person data extracting...\n")
-    tm1 <- as.numeric(round(system.time(persontbl <- person_data(connection))[3], digit = 1))
-    rds_maker(work_dir,persontbl,"person")
-    ################################################################################
-    # DEATH DATA SAVING FUNCTION
-    ################################################################################
-    cat("\nDeath data extracting...\n")
-    tm2 <- as.numeric(round(system.time(deathtbl <- death_data(connection))[3], digit = 1))
-    rds_maker(work_dir,deathtbl,"death")
-    ################################################################################
-    # VISIT_OCCURRENCE DATA SAVING FUNCTION
-    ################################################################################
-    cat("\nVisit occurrence data extracting...\n")
-    tm3 <- as.numeric(round(system.time(visittbl <- visit_occurrence_data(connection))[3], digit = 1))
-    rds_maker(work_dir,visittbl,"visit")
-    ################################################################################
-    # CONDITION_OCCURRENCE DATA SAVING FUNCTION
-    ################################################################################
-    cat("\nCondition occurrence data extracting...\n")
-    tm4 <- as.numeric(round(system.time(conditiontbl <- condition_occurrence_data(connection))[3], digit = 1))
-    rds_maker(work_dir,conditiontbl,"condition")
-    ################################################################################
-    # DRUG_EXPOSURE DATA SAVING FUNCTION
-    ################################################################################
-    cat("\nDrug exposure data extracting...\n")
-    tm5 <- as.numeric(round(system.time(exptbl <- drug_exposure_data(connection))[3], digit = 1))
-    rds_maker(work_dir,exptbl,"drug_exposure")
-    ################################################################################
-    # DRUG_ERA DATA SAVING FUNCTION
-    ################################################################################
-    cat("\nDrug era data extracting...\n")
-    tm6 <- as.numeric(round(system.time(eratbl <- drug_era_data(connection))[3], digit = 1))
-    rds_maker(work_dir,eratbl,"drug_era")
+save_data <- function(connection, workDir, schemaName){
+    dir.create(file.path(workDir, "Gemini RDS"), showWarnings = FALSE)
+    workDir <- file.path(workDir, "Gemini RDS","/")
+    table_name <- c('person','death','visit_occurrence','condition_occurrence','drug_exposure','drug_era')
+    process_time <- sapply(table_name, function(x){extract_cdm(connection,workDir,x)})
 
     ################################################################################
     # File Saving
     ################################################################################
 
-    zip(zipfile = paste0(work_dir,cdmDatabaseSchema,".zip"),
-        files = paste0(work_dir,list.files(path = work_dir,pattern = "\\w*.rds$")), flag= c("-j", "-r"))
+    zip(zipfile = paste0(workDir,schemaName,".zip"),
+        files = paste0(workDir,list.files(path = workDir,pattern = "\\w*.rds$")), flag= c("-j", "-r"))
 
-    if(length(list.files(path = work_dir, pattern = "\\w.zip$"))>0){
-        file.remove(paste0(work_dir,list.files(path = work_dir, pattern = "\\w.rds$")))
+    if(length(list.files(path = workDir, pattern = "\\w.zip$"))>0){
+        file.remove(paste0(workDir,list.files(path = workDir, pattern = "\\w.rds$")))
     }
     # time check
-    cat(paste0("RDS files created.\nThis process takes ", sum(tm1, tm2, tm3, tm4, tm5, tm6), "s.\n"))
+    cat(paste0("RDS files created.\nThis process takes ", sum(process_time), "s.\n"))
     ################################################################################
     # Disconnect DB
     ################################################################################
     DatabaseConnector::disconnect(connection)
 }
 
-rds_maker <- function(work_dir,tbl,table_name){
-    switch (table_name,
+extract_cdm <- function(connection, workDir, tableName){
+    cat(paste("\n",tableName,"data extracting...\n"))
+    switch(tableName,
+           'person' = {
+               tm <- as.numeric(round(system.time(persontbl <- person_data(connection))[3], digit = 1))
+               rds_maker(workDir,persontbl,tableName)
+           },
+           'death' = {
+               tm <- as.numeric(round(system.time(deathtbl <- death_data(connection))[3], digit = 1))
+               rds_maker(workDir,deathtbl,tableName)
+           },
+           'visit_occurrence' = {
+               tm <- as.numeric(round(system.time(visittbl <- visit_occurrence_data(connection))[3], digit = 1))
+               rds_maker(workDir,visittbl,tableName)
+           },
+           'condition_occurrence' = {
+               tm <- as.numeric(round(system.time(conditiontbl <- condition_occurrence_data(connection))[3], digit = 1))
+               rds_maker(workDir,conditiontbl,tableName)
+           },
+           'drug_exposure' = {
+               tm <- as.numeric(round(system.time(exptbl <- drug_exposure_data(connection))[3], digit = 1))
+               rds_maker(workDir,exptbl,tableName)
+           },
+           'drug_era' = {
+               tm <- as.numeric(round(system.time(eratbl <- drug_era_data(connection))[3], digit = 1))
+               rds_maker(workDir,eratbl,tableName)
+           }
+    )
+    return(tm)
+}
+
+rds_maker <- function(workDir,tbl,tableName){
+    switch (tableName,
             "person" =  {
                 persontbl_name_list <- c(
                     "persontbl_record", "persontbl_person_ratio", "persontbl_gender", "persontbl_min_age", "persontbl_max_age",
@@ -90,15 +89,15 @@ rds_maker <- function(work_dir,tbl,table_name){
                 )
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir,persontbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir,persontbl_name_list, ".rds")
             },
             "death" = {
                 deathtbl_name_list <- c("deathtbl_check", "deathtbl_type")
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir, deathtbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir, deathtbl_name_list, ".rds")
             },
-            "visit" = {
+            "visit_occurrence" = {
                 visittbl_name_list <- list(
                     "visittbl_record", "visittbl_person_ratio", "visittbl_visit_concept", "visittbl_start", "visittbl_end",
                     "visittbl_diff_date", "visittbl_count", "visittbl_type_concept", "visittbl_care_site", "visittbl_source_concept",
@@ -106,9 +105,9 @@ rds_maker <- function(work_dir,tbl,table_name){
                 )
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir, visittbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir, visittbl_name_list, ".rds")
             },
-            "condition" = {
+            "condition_occurrence" = {
                 conditiontbl_name_list <- list(
                     "conditiontbl_record", "conditiontbl_person_ratio", "conditiontbl_diff_date", "conditiontbl_start",
                     "conditiontbl_end", "conditiontbl_type_concept", "conditiontbl_stop", "conditiontbl_visit_occurrence",
@@ -116,7 +115,7 @@ rds_maker <- function(work_dir,tbl,table_name){
                 )
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir, conditiontbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir, conditiontbl_name_list, ".rds")
             },
             "drug_exposure" = {
                 drug_exptbl_name_list <- list(
@@ -125,7 +124,7 @@ rds_maker <- function(work_dir,tbl,table_name){
                 )
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir, drug_exptbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir, drug_exptbl_name_list, ".rds")
             },
             "drug_era" = {
                 drug_eratbl_name_list <- list(
@@ -134,7 +133,7 @@ rds_maker <- function(work_dir,tbl,table_name){
                 )
 
                 data_list <- tbl
-                rds_path <- sapply(FUN = paste0, work_dir, drug_eratbl_name_list, ".rds")
+                rds_path <- sapply(FUN = paste0, workDir, drug_eratbl_name_list, ".rds")
             }
     )
     mapply(saveRDS, object = data_list, file = rds_path)
