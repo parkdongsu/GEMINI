15a808513cbd24fa0e405fcf5ea68e61e0683ca1
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2020-02-05 10:07:35 +0900

    G temp (#27)
    * pie graphic issue solve
    
    * change method of setting work dir
    
    * resolve rds person min age bug

--- extras/codeToCreate.R
+++ extras/codeToCreate.R
@@ -1,13 +1,47 @@
 # CodeToRun
 #usethis::edit_r_environ() ## edit ID/PW
-
-
+### work_dir <- readline(prompt="Enter work_dir: ")
+##16호 AUSOM-----------------------------------------------------------------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
+                                                                server = Sys.getenv("server_ip_16"),
+                                                                user = Sys.getenv("chungsu_ID"),
+                                                                password = Sys.getenv("chungsu_pW"),
+                                                                schema = Sys.getenv("AUSOMv5_3"))
+create_rds(connectionDetails)
+##16호 AUSOM-----------------------------------------------------------------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
+                                                                server = Sys.getenv("server_ip_16"),
+                                                                user = Sys.getenv("chungsu_ID"),
+                                                                password = Sys.getenv("chungsu_pW"),
+                                                                schema = Sys.getenv("AUSOMv5_3_1"))
+create_rds(connectionDetails)
+##17호 CDMPv1----------------------------------------------------------------------------------------
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
                                                                 server = Sys.getenv("server_ip_17"),
                                                                 user = Sys.getenv("USER_ID"),
-                                                                password = Sys.getenv("PASSWORD_17"),
-                                                                schema = Sys.getenv("ICARUS")
-                                                                )
+                                                                password = Sys.getenv("PASSWORD"),
+                                                                schema = Sys.getenv("CDMPv1"))
+create_rds(connectionDetails)
+##17호 Dolphin_CDM-----------------------------------------------------------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
+                                                                server = Sys.getenv("server_ip_17"),
+                                                                user = Sys.getenv("USER_ID"),
+                                                                password = Sys.getenv("PASSWORD"),
+                                                                schema = Sys.getenv("Dolphin_CDM"))
+create_rds(connectionDetails)
+##17호 synPUF_CDM------------------------------------------------------------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
+                                                                server = Sys.getenv("server_ip_17"),
+                                                                user = Sys.getenv("USER_ID"),
+                                                                password = Sys.getenv("PASSWORD"),
+                                                                schema = Sys.getenv("synPUF_CDM"))
+create_rds(connectionDetails)
+##17호 ICARUS----------------------------------------------------------------------------------------
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = 'sql server',
+                                                                server = Sys.getenv("server_ip_17"),
+                                                                user = Sys.getenv("USER_ID"),
+                                                                password = Sys.getenv("PASSWORD"),
+                                                                schema = Sys.getenv("ICARUS"))
 create_rds(connectionDetails)
 
 gemini::gemini(dbCount=2)
